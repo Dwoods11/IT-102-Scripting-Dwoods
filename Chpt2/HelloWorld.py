1d116725ef6b6e887d1f6fa9cf531a74921f4695
Dwoods11 <Woods.Devante@student.greenriver.edu>
2025-09-30 09:46:39 -0700

    Hello world chapter 2

---
+++ Chpt2/HelloWorld.py
@@ -0,0 +1,16 @@
+#Prompts the user for their name
+name = input ("Please enter your name:")
+
+positive_affirmation = "This quarter is going to be the best"
+#Print hello name?
+print("name")
+
+#Print a positive affirmation
+print(positive_affirmation)
+print("This is going to be amazing! ")
+
+# How do we prompt the user for there age?
+age = int(input("what is your age? "))
+
+# Print out in 2 years you will be xx years old
+print(age + 2)
